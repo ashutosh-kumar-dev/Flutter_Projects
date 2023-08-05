18671fc820a4c6660548b8e28ab519a2a73905f8
ashutosh kumar <turboblitz.dev@gmail.com>
2023-08-05 15:57:31 +0530

    new

---
+++ listView/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    var arrNames = [Colors.orange,Colors.yellow,Colors.orange];
+
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        itemBuilder: ((context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Colors.orange,
+            height: 200,
+            width: 200,
+          ),
+        );
+      }),
+        itemCount: 5,
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: 5,
+            thickness: 5,
+
+          );
+        },
+      )
+
+
+
+    );
+  }
+}
